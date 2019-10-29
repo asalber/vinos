26a558dc2c7e88df82c6e284c4359c04bfa4a821
Alfredo Sánchez Alberca <asalber@gmail.com>
2019-10-29 19:53:55 +0100

    Corrección de errores.

--- inst/rkward/plugins/homework.js
+++ inst/rkward/plugins/homework.js
@@ -15,16 +15,13 @@ function calculate () {
 	echo('datos.vinos$acidez.volatil <- modify.unif(datos.vinos$acidez.volatil, -0.02, 0.02, 2)\n');
 	echo('datos.vinos$acido.citrico <- modify.unif(datos.vinos$acido.citrico, -0.02, 0.02, 2)\n');
 	echo('datos.vinos$azucar.residual <- modify.unif(datos.vinos$azucar.residual, -0.2, 0.2, 1)\n');
-	echo('datos.vinos$acidez.volatil <- modify.unif(datos.vinos$acidez.volatil, -0.02, 0.02, 2)\n');
 	echo('datos.vinos$cloruro.sodico <- modify.unif(datos.vinos$cloruro.sodico, -0.002, 0.002, 3)\n');
-	echo('datos.vinos$acidez.volatil<- modify.unif(datos.vinos$acidez.volatil, -0.02, 0.02, 2)\n');
 	echo('datos.vinos$dioxido.azufre.libre <- modify.unif(datos.vinos$dioxido.azufre.libre, -2, 2, 0)\n');
 	echo('datos.vinos$dioxido.azufre.total <- modify.unif(datos.vinos$dioxido.azufre.total, -5, 5, 0)\n');
 	echo('datos.vinos$densidad <- modify.unif(datos.vinos$densidad, -0.0002, 0.0002, 4)\n');
 	echo('datos.vinos$ph <- modify.unif(datos.vinos$ph, -0.01, 0.01, 2)\n');
 	echo('datos.vinos$sulfatos <- modify.unif(datos.vinos$sulfatos, -0.01, 0.01, 2)\n');
 	echo('datos.vinos$alcohol <- modify.unif(datos.vinos$alcohol, -0.1, 0.1, 1)\n');
-	echo('datos.vinos$dioxido.azufre.libre <- modify.unif(datos.vinos$dioxido.azufre.libre, -2, 2, 0)\n');
 	echo('.GlobalEnv$datos.vinos.' + dni + '<- datos.vinos\n');
 	echo('rm(datos.vinos, envir=.GlobalEnv)\n');
 }
